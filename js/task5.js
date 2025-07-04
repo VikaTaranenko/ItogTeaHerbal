037b48ca88ddb56fc405f78b65b6e342db486fc1
VikaTaranenko <pikyseva@mail.ru>
2025-07-04 19:11:26 +1000

    Add files via upload

--- js/task5.js
+++ js/task5.js
@@ -1,26 +1,11 @@
-const BigImage = document.querySelector("#big-image")
-const img1 = document.querySelector("#img-1")
-const img2 = document.querySelector("#img-2")
-const img3 = document.querySelector("#img-3")
-const img4 = document.querySelector("#img-4")
-const img5 = document.querySelector("#img-5")
+const burgerMenuBtn = document.querySelector("#burger-menu-btn")
+const burgerMenu = document.querySelector("#burger-menu")
+const burgerMenuClose = document.querySelector("#burger-menu-close")
 
-img1.addEventListener("click", () => {
-BigImage.src = img1.src
+burgerMenuBtn.addEventListener("click", () => {
+    burgerMenu.style.left = "0px"
 })
 
-img2.addEventListener("click", () => {
-BigImage.src = img2.src
-})
-
-img3.addEventListener("click", () => {
-BigImage.src = img3.src
-})
-
-img4.addEventListener("click", () => {
-BigImage.src = img4.src
-})
-
-img5.addEventListener("click", () => {
-BigImage.src = img5.src
+burgerMenuClose.addEventListener("click", () => {
+    burgerMenu.style.left = "-400px"
 })
